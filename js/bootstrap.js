3755b9fc93382c11c0c225192c75ac4a1c02982a
imrahn <imrahnr@gmail.com>
2015-04-27 19:03:24 -0600

    changed js back to original

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -322,7 +322,7 @@ if (typeof jQuery === 'undefined') {
 
   Carousel.VERSION  = '3.3.4'
 
-  Carousel.TRANSITION_DURATION = 400
+  Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
     interval: 5000,
